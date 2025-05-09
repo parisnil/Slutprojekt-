da6ae38b8b556eda8c3fc6897b0117d6ccd35842
Paris <paris.mengnilsson@skola.taby.se>
2025-05-09 12:42:09 +0200

    Update projekt.js

--- projekt.js
+++ projekt.js
@@ -26,6 +26,30 @@ document.addEventListener("keyup", (e) => {
   if (e.key in keys) keys[e.key] = false;
 });
 
+let obstacles = [];
+
+function getRandomColor() {
+  const colors = ["red", "green", "blue", "purple", "orange", "pink"];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function createObstacle() {
+  const height = 40 + Math.random() * 50;
+  const y = Math.random() * (canvas.height - height);
+  const color = getRandomColor();
+
+  obstacles.push({
+    x: canvas.width,
+    y,
+    width: 30,
+    height,
+    color,
+  });
+
+  const nextInterval = 500 + Math.random() * 1000;
+  setTimeout(createObstacle, nextInterval);
+}
+
 function drawFish() {
   const gradient = ctx.createLinearGradient(
     fish.x - 20,
@@ -76,18 +100,64 @@ function drawFish() {
   ctx.fillStyle = "#8400ff";
   ctx.fill();
 
- let eye;
-  if (fish.direction === "right") {
-    eye = 10;
+  let eyeOffset = fish.direction === "right" ? 10 : -10;
+  const eyeX = fish.x + eyeOffset;
+  const eyeY = fish.y - 5;
+
+  if (gameOver) {
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 4;
+    ctx.beginPath();
+    ctx.moveTo(eyeX - 7, eyeY - 7);
+    ctx.lineTo(eyeX + 7, eyeY + 7);
+    ctx.moveTo(eyeX + 7, eyeY - 7);
+    ctx.lineTo(eyeX - 7, eyeY + 7);
+    ctx.stroke();
   } else {
-    eye = -10;
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(eyeX, eyeY, 10, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(eyeX, eyeY, 7, 0, Math.PI * 2);
+    ctx.fill();
   }
-  ctx.fillStyle = "white";
-  ctx.beginPath();
-  ctx.arc(fish.x + eye, fish.y - 5, 10, 0, Math.PI * 2);
-  ctx.fill();
-  ctx.fillStyle = "black";
-  ctx.beginPath();
-  ctx.arc(fish.x + eye, fish.y - 5, 7, 0, Math.PI * 2);
-  ctx.fill();
 }
+
+function drawObstacle(ob) {
+  ctx.fillStyle = ob.color; 
+  ctx.fillRect(ob.x, ob.y, ob.width, ob.height);
+}
+
+function collision(ob) {
+  return (
+    fish.x + fish.width > ob.x &&
+    fish.x - fish.width < ob.x + ob.width &&
+    fish.y + fish.height > ob.y &&
+    fish.y - fish.height < ob.y + ob.height
+  );
+}
+
+for (let i = 0; i < obstacles.length; i++) {
+    const ob = obstacles[i];
+    ob.x -= 2;
+    drawObstacle(ob);
+    if (collision(ob)) {
+      gameOver = true;
+    }
+  }
+  obstacles = obstacles.filter((ob) => ob.x + ob.width > 0);
+
+function gameEnd() {
+  if (gameOver) {
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.font = "48px Arial";
+    ctx.fillText("Game Over", canvas.width / 2 - 120, canvas.height / 2);
+    return;
+  }
+  
+gameEnd();
